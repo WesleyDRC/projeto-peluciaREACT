f13a1db234174875652a67d7c429d89aac51bc24
Wesley <wesleymiranda04b@gmail.com>
2022-03-28 11:02:11 -0300

    Requisição
    Trocando fetch pelo uso do axios

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,43 +1,48 @@
-import ChangeImage from '../layout/ChangeImage';
-import PeluciaCard from '../PeluciaCard/PeluciaCard';
-import Api from '../../services/api';
+import ChangeImage from "../layout/ChangeImage";
+import PeluciaCard from "../PeluciaCard/PeluciaCard";
+import api from "../../services/api";
 import { useEffect, useState } from "react";
-import axios from "axios";
 
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
 
-function Home (){
-    const [card, setCard] = useState([])
-    useEffect(() => {
-          fetch("https://9qz2iwilyc.execute-api.sa-east-1.amazonaws.com/dev/products", {
-            method: "GET",
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              setCard(data);
-            })
-            .catch((error) => console.log(error));
-      });
+function Home() {
+  const [card, setCard] = useState([]);
 
-    return(
-        <>
-        <ChangeImage />
-        <div className={styles.container}>
-            {card.length > 0 &&
-                card.map( (card) => (
-                    <PeluciaCard 
-                    name={card.name}
-                    price={card.price}
-                    imageUrl={card.imageUrl}
-                    size = {card.size}
-                    measure={card.measure}
-                    />
-                ))
-            }
-        </div>
-        </>
-    )
+  useEffect(() => {
+    const getPets = async () => {
+      const response = await api.get("/products");
+
+      setCard(response.data);
+    };
+    getPets();
+  }, []);
+
+  return (
+    <>
+      <ChangeImage />
+      <div className={styles.container}>
+        {card.length > 0 &&
+          card.map((card) => (
+            <PeluciaCard
+              name={card.name}
+              price={card.price}
+              imageUrl={card.imageUrl}
+              size={card.size}
+              measure={card.measure}
+            />
+          ))}
+      </div>
+    </>
+  );
 }
 
-
 export default Home;
+
+// fetch("https://9qz2iwilyc.execute-api.sa-east-1.amazonaws.com/dev/products", {
+//             method: "GET",
+//           })
+//             .then((response) => response.json())
+//             .then((data) => {
+//               setCard(data);
+//             })
+//             .catch((error) => console.log(error));
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 
-function Api() {
-    const url = `https://9qz2iwilyc.execute-api.sa-east-1.amazonaws.com/dev/products`;
-    return (
-        axios
-            .get(url)
-            .then ( response => response.data )
-            .catch(error => console.log(error))
-    )
-}
+const api = axios.create({
+    baseURL: 'https://9qz2iwilyc.execute-api.sa-east-1.amazonaws.com/dev',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
 
-export default Api    
+export default api    
+
+
