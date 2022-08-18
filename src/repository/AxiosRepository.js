f564173c356e339c3c9f6be7896469b86e552cd0
Matheus <44927173+MatheusCoxxxta@users.noreply.github.com>
2022-08-18 14:48:06 -0300

    feat: using public attr

---
+++ src/repository/AxiosRepository.js
@@ -0,0 +1,16 @@
+import { api } from '../services/api'
+
+class AxiosRepository {
+
+   axiosClient;
+  
+    constructor () {
+       this.axiosClient = api;
+    }
+  
+    async findAll() {
+       return this.axiosClient.get('/products');
+    }
+  }
+  
+  export default new AxiosRepository();
