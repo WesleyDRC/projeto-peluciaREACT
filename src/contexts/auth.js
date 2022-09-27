128819f90e608b9eb4fad7c2c7e2bd466c8ef595
Wesley <wesleymiranda04b@gmail.com>
2022-09-27 11:17:37 -0300

    update repository
    Creating api methods/routes.

---
+++ src/contexts/auth.js
@@ -0,0 +1,64 @@
+import { createContext, useState, useEffect } from "react";
+
+import AxiosRepository from '../repository/AxiosRepository';
+import { api } from "../services/api";
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("user_token");
+
+    if (userToken) {
+      api.defaults.headers.Authorization = `Bearer ${userToken}`;
+      setUser(JSON.parse(userToken));
+    }
+  }, []);
+
+  // star login
+  const SignIn = async (email, password) => {
+    try {
+      const response = await AxiosRepository.signIn({ email, password });
+      localStorage.setItem("user_token", JSON.stringify(response.data.token));
+      api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+      setUser(response.data.token);
+    } catch (error) {
+      if(error.response.status !== error.response.status.ok) {
+        return (error.response.data.message)
+      }
+    }
+  };
+  // end login
+
+  const SignUp = async (name, email, password) => {
+
+    try {
+      const response = await AxiosRepository.signUp({
+        name,
+        email,
+        password,
+      });
+      localStorage.setItem("user_token", JSON.stringify(response.data.token));
+    } catch (error) {
+      if(error.response.status !== error.response.status.ok) {
+        return (error.response.data.message)
+      }
+    }
+  };
+
+  const SignOut = () => {
+    setUser(null);
+    api.defaults.headers.Authorization = null;
+    localStorage.removeItem("user_token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signed: !!user, SignIn, SignUp, SignOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+<<<<<<< Updated upstream
 const baseURL = process.env.REACT_APP_BASE_URL_
+=======
+// const baseURL = process.env.REACT_APP_BASE_URL;
+const baseURL = "http://localhost:3333"
+>>>>>>> Stashed changes
 
 export const api = axios.create({
   baseURL,
