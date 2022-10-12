8d960e4efd474d7ba7d79deaf261b090a640ce3d
Wesley <wesleymiranda04b@gmail.com>
2022-10-12 17:39:49 -0300

    Update FinalizeOrder.js

--- src/components/pages/FinalizeOrder.js
+++ src/components/pages/FinalizeOrder.js
@@ -148,7 +148,6 @@ export default function FinalizeOrder() {
 		var theEvent = evt || window.event;
 		var key = theEvent.keyCode || theEvent.which;
 		key = String.fromCharCode( key );
-		//var regex = /^[0-9.,]+$/;
 		var regex = /^[0-9]+$/;
 		if( !regex.test(key) ) {
 			 theEvent.returnValue = false;
