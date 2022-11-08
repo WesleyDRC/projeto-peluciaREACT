8d8f2fe524c5e419f98be444a08a6e35ac4d2162
Wesley <wesleymiranda04b@gmail.com>
2022-11-08 15:40:32 -0300

    Estilização da dashboard do usuário.

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import styles from "./Dashboard.module.css";
 import useAuth from "../../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import avatar from "./img/avatar.png";
+
+import { AiOutlineEdit } from "react-icons/ai";
 
 function DashBoard() {
   const { SignOut } = useAuth();
@@ -9,16 +12,31 @@ function DashBoard() {
   return (
     <>
       <div className={styles.container}>
-        <nav className={styles.navBar}>
-          <ul>
-            <li>Minha Conta</li>
-          </ul>
-        </nav>
-        <div className={styles.title}>
-          <h1> Minha conta </h1>
-          <p>Você está lógado como:  </p>
-        </div>
+        <div>
+          <div className={styles.boxUser}>
+            <Link to="/">
+              <div className={styles.peluciaAvatar}>
+                <img src={avatar} />
+              </div>
+            </Link>
+            <div className={styles.profile}>
+              <p className={styles.username}> Wesley Luis </p>
+              <Link to="/">
+                <p>
+                  <AiOutlineEdit /> Editar perfil
+                </p>
+              </Link>
+            </div>
+          </div>
 
+          <div className={styles.listDashboard}>
+            <nav className={styles.navBar}>
+              <ul>
+                <li>Minha Conta</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
 
         <button
           title="SignOut"
